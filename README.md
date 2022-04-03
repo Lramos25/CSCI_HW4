cef64cd8d11df9cb5fd47d47b8e3c2f60b2729bf
Lramos25 <luis.ramos_79@yahoo.com>
2022-04-03 12:53:09 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+"# CSC_3619HW2" 
+
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/19231130-64b04af9-ef16-4941-a6bf-e12296353b81?action=collection%2Ffork&collection-url=entityId%3D19231130-64b04af9-ef16-4941-a6bf-e12296353b81%26entityType%3Dcollection%26workspaceId%3D059b02d9-6ab5-4182-b56c-f56e5f01f6f4)
+
+
+
+# **Working 100%:*
+
+## **Signup Fail:**
+![signup fail](https://user-images.githubusercontent.com/10605443/157997828-c785155e-5830-4389-b598-4710ba5001a7.png)
+
+
+## **Signup Success:**
+![signup good](https://user-images.githubusercontent.com/10605443/157997827-81d47673-7887-48e3-8761-997a3ab8eada.png)
+
+
+## **Login Fail username:**
+![signin fail username](https://user-images.githubusercontent.com/10605443/157997831-b8eaf727-e0d7-4d9b-be00-904786606e68.png)
+
+
+## **Login Fail password:**
+![signin fail password](https://user-images.githubusercontent.com/10605443/157997830-a36f3c94-381b-40b5-80c0-bf506c235a24.png)
+
+
+## **Login Fail request type:**
+![signin fail method](https://user-images.githubusercontent.com/10605443/157997832-a2c92a59-e37b-48b2-abbe-35fd4ae029bf.png)
+
+
+## **Login Success:**
+![signin good](https://user-images.githubusercontent.com/10605443/157997829-954e0c70-f522-485e-9c22-55947e4dc36d.png)
+
+
+## **GetMovie Fail:**
+![get movie fail](https://user-images.githubusercontent.com/10605443/157997835-45b5dad4-ce76-4483-8dab-0bbe6d86c0dd.png)
+
+
+## **GetMovie Success:**
+![get movie good](https://user-images.githubusercontent.com/10605443/157997833-ada3130b-4ad2-4f8c-b1d9-a1dc1b95f79f.png)
+
+
+## **PostMovie Fail:**
+![post movie fail](https://user-images.githubusercontent.com/10605443/157997837-462ae470-5348-4616-b165-e8c0a7998ed9.png)
+
+
+## **PostMovie Success:**
+![post movie good](https://user-images.githubusercontent.com/10605443/157997836-f676dff0-b02e-480d-a718-f6544785f093.png)
+
+## PutMovie fail with incorrect token
+![put movie fail](https://user-images.githubusercontent.com/10605443/157997841-f24d7a11-8080-488a-af93-1bbd56bea612.png)
+
+## PutMovie success with token
+![put movie good](https://user-images.githubusercontent.com/10605443/157997839-68dabf4d-ba41-4fe0-9499-3a17cd508774.png)
+
+## **DeleteMovie Fail:**
+![delete movie fail](https://user-images.githubusercontent.com/10605443/157997843-19b9ec5f-a6ca-407d-9057-8975ad50fec0.png)
+
+
+## **DeleteMovie Success:**
+![delete movie good](https://user-images.githubusercontent.com/10605443/157997842-fa13aacd-f006-4433-b857-d7a85b930b02.png)
+
+
+## **Request(PATCH) Fail:**
+![patch method](https://user-images.githubusercontent.com/10605443/157998072-12b6a8f1-59b3-4c57-8d6c-558dbb96f6cd.png)
+
+
+
+
---
+++ server.js
@@ -0,0 +1,334 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cors = require('cors');
+
+const authJwtController = require('./auth_jwt');
+const jwt = require('jsonwebtoken');
+const User = require('./Users');
+const Movie = require("./Movies");
+const Review = require("./Review");
+
+const rp = require('request-promise');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(passport.initialize());
+const router = express.Router();
+
+function getJSONObjectForMovieRequirement(req, msg)
+{
+    let json =
+        {
+            message: msg,
+            headers: "No headers",
+            key: process.env.UNIQUE_KEY,
+            body: "No body"
+        };
+
+    if (req.body != null)
+    {
+        json.body = req.body;
+    }
+
+    if (req.headers != null)
+    {
+        json.headers = req.headers;
+    }
+
+    return json;
+}
+
+const GA_TRACKING_ID = process.env.GA_KEY;
+
+function trackDimension(category, action, label, value, dimension, metric) {
+
+    var options = { method: 'GET',
+        url: 'https://www.google-analytics.com/collect',
+        qs:
+            {
+                v: '1',
+                tid: GA_TRACKING_ID,
+                cid: crypto.randomBytes(16).toString("hex"),
+                // Event type, category, action, label, value, dimension, metric
+                t: 'event',
+                ec: category,
+                ea: action,
+                el: label,
+                ev: value,
+                cd1: dimension,
+                cm1: metric
+            },
+        headers:
+            {  'Cache-Control': 'no-cache' } };
+
+    return rp(options);
+}
+
+router.post('/signup', function (req, res)
+{
+    if (!req.body.username || !req.body.password)
+    {
+        res.json({success: false, msg: 'Please include both username and password to signup.'})
+    }
+    else
+    {
+        let user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.save(function (err)
+        {
+            if (err)
+            {
+                if (err.code === 11000)
+                    return res.json({success: false, message: 'A user with that username already exists.'});
+                else
+                    return res.json(err);
+            }
+            res.json({success: true, msg: 'Successfully created new user.'})
+        });
+    }
+});
+
+router.post('/signin', function (req, res)
+{
+    let userNew = new User();
+    userNew.username = req.body.username;
+    userNew.password = req.body.password;
+    User.findOne({username: userNew.username}).select('name username password').exec(function (err, user)
+    {
+        if (err)
+        {
+            res.send(err);
+        }
+        user.comparePassword(userNew.password, function (isMatch)
+        {
+            if (isMatch)
+            {
+                let userToken = {id: user.id, username: user.username};
+                let token = jwt.sign(userToken, process.env.SECRET_KEY, null, null);
+                res.json({success: true, token: 'JWT ' + token});
+            } else {
+                res.status(401).send({success: false, msg: 'Authentication failed.'});
+            }
+        })
+    })
+});
+
+router.route('/movies')
+    .get(authJwtController.isAuthenticated, function (req, res)
+    {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type'))
+        {
+            res = res.type(req.get('Content-Type'));
+        }
+        Movie.find().exec(function (err, movies)
+        {
+            if (err)
+            {
+                res.send(err);
+            }
+            if (movies.length < 1)
+            {
+                res.json({success: false, message: 'No movies found.'});
+            }
+            else
+            {
+                res.json(movies);
+            }
+        })
+    })
+
+    .post(authJwtController.isAuthenticated, function (req, res) // genres idea form traversy media (youyube)
+    {
+        console.log(req.body);
+        res = res.status(200);
+        const genres =
+            ["Action",
+                "Anime",
+                "Adventure",
+                "Comedy",
+                "Drama",
+                "Fantasy",
+                "Horror",
+                "Mystery",
+                "Suspense",
+                "Thriller"];
+        if(!req.body.title){res.json({success: false, message: "Title Missing"});}
+        else if (!req.body.genre)
+        {
+            res.json({success: false, message: 'Title Missing.'})
+        }
+        else if (!genres.includes(req.body.genre))
+        {
+            res.json({success: false, message: "Genre Missing.", accepted_genres: genres})
+        }
+        else if (!req.body.yearReleased)
+        {
+            res.json({success: false, message: 'Missing Year YYYY.'})
+        }
+        else if (req.body.actors.length < 3)
+        {
+            res.json({success: false, message: 'Must include at least 3 Actors.'})
+        }
+        else {
+            let movieNew = new Movie();
+            movieNew.title = req.body.title;
+            movieNew.yearReleased = req.body.yearReleased;
+            movieNew.genre = req.body.genre;
+            movieNew.actors = req.body.actors;
+
+            if (req.get('Content-Type'))
+            {
+                res = res.type(req.get('Content-Type'));
+            }
+
+            movieNew.save(function (err)
+            {
+                if (err) {
+                    if (err.code === 11000)
+                        return res.json({success: false, message: 'This Movie already exists.'});
+                    else
+                        return res.json(err);
+                } else {
+                    var o = getJSONObjectForMovieRequirement(req, 'Movie has been saved');
+                    res.json(o)
+                }
+            });
+        }
+    })
+
+
+//idea from Ayan Tuladhar and https://javascript.plainenglish.io/using-nested-routes-in-react-22f9d3fbcf06
+router.route('/movies/:title') //able to read the different dynamic segments within our component using the ":" with the segment we want
+    .get(authJwtController.isAuthenticated, function (req, res)
+    {
+        console.log(req.body);
+        res = res.status(200);
+
+        if (req.get('Content-Type'))
+        {
+            res = res.type(req.get('Content-Type'));
+        }
+        Movie.find({title: req.params.title}).exec(function (err, movie)
+        {
+            if (err)
+            {
+                res.send(err);
+            }
+            res.json(movie);
+        })
+    })
+    .delete(authJwtController.isAuthenticated, function (req, res)
+    {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type'))
+        {
+            res = res.type(req.get('Content-Type'));
+        }
+        Movie.find({title: req.params.title}).exec(function (err, movie) {
+            if (err)
+            {
+                res.send(err);
+            }
+            console.log(movie);
+            if (movie.length < 1)
+            {
+                res.json({success: false, message: 'Movie Title not found.'});
+            } else
+            {
+                Movie.deleteOne({title: req.params.title}).exec(function (err)
+                {
+                    if (err)
+                    {
+                        res.send(err);
+                    } else
+                    {
+                        var o = getJSONObjectForMovieRequirement(req, 'Movie deleted');
+                        res.json(o);
+                    }
+                })
+            }
+        })
+    })
+    .put(authJwtController.isAuthenticated, function (req, res)
+    {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type'))
+        {
+            res = res.type(req.get('Content-Type'));
+        }
+        Movie.updateOne({title: req.params.title},
+            {
+                title: req.body.title,
+                yearReleased: req.body.yearReleased, genre: req.body.genre, actors: req.body.actors
+            })
+            .exec(function (err)
+            {
+                if (err)
+                {
+                    res.send(err);
+                }
+            })
+        var o = getJSONObjectForMovieRequirement(req, 'Movie updated');
+        res.json(o);
+    });
+
+
+router.route('/reviews')
+    .post(authJwtController.isAuthenticated, function(req, res) {
+        if (!req.body.small_quote || !req.body.rating || !req.body.title)
+        {
+            return res.json({ success: false, message: 'Please include all information (small quote, rating, and title of movie)'});
+        }
+        else {
+            var review = new Review();
+
+            // warning states invalid number of arguments, expects 4 got 3. Looking at documentation jwt.verify needs two and can take three(symmetric)
+            jwt.verify(req.headers.authorization.substring(4), process.env.SECRET_KEY, function(err, ver_res) {
+                if (err)
+                {
+                    return res.status(403).json({success: false, message: "Unable to post review"});
+                } else {
+                    review.user_id = ver_res.id;
+
+                    Movie.findOne({title: req.body.title}, function(err, movie) {
+                        if (err) {
+                            return res.status(403).json({success: false, message: "Unable to post review"});
+                        } else if (!movie) {
+                            return res.status(403).json({success: false, message: "Unable to find movie"});
+                        } else {
+                            review.movie_id = movie._id;
+                            review.username = ver_res.username;
+                            review.small_quote = req.body.small_quote;
+                            review.rating = req.body.rating;
+
+                            review.save (function (err) {
+                                if (err) {
+                                    return res.status(403).json({success: false, message: "Unable to post review"});
+                                } else {
+                                    trackDimension(movie.genre, 'post/review', 'POST', review.rating, movie.title, '1');
+
+                                    return res.status(200).json({success: true, message: "Review posted", movie: movie});
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    });
+
+
+
+
+
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
+module.exports = app; // for testing only
