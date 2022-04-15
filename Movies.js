1debc05754102f95beb3b6dd75bfcc123d1b6929
Lramos25 <luis.ramos_79@yahoo.com>
2022-04-15 15:35:59 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,27 +1,48 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+require("dotenv").config()
 mongoose.Promise = global.Promise;
 
 //mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try
-{
+try {
     mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
         console.log("connected"));
-}catch (error)
-{
+}catch (error) {
     console.log("could not connect");
 }
 //mongoose.set('useCreateIndex', true);
 
-//movies schema
-let MovieSchema = new Schema({
-    title: {type: String, required: true, index: { unique: true}},
-    yearReleased: {type: String, required: true},
-    genre: {type: String, required: true},
-    actors: [{actorName: String, characterName: String}]
+//user schema
+var UserSchema = new Schema({
+    name: String,
+    username: { type: String, required: true, index: { unique: true }},
+    password: { type: String, required: true, select: false }
 });
 
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    //hash the password
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        //change the password
+        user.password = hash;
+        next();
+    });
+});
+
+UserSchema.methods.comparePassword = function (password, callback) {
+    var user = this;
+
+    bcrypt.compare(password, user.password, function(err, isMatch) {
+        callback(isMatch);
+    })
+}
 
 //return the model to server
-module.exports = mongoose.model('Movie', MovieSchema);
+module.exports = mongoose.model('User', UserSchema);
