358f6d54c64d37f5f7f77fadf856bcf4fb5a2bdb
Lramos25 <luis.ramos_79@yahoo.com>
2022-04-15 15:35:47 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,62 +1,27 @@
-'use strict;';
-//Include crypto to generate the movie id
-var crypto = require('crypto');
-module.exports = function () {
-    return {
-        userList: [],
 
-        save: function (user) {
-            user.id = crypto.randomBytes(20).toString('hex'); // fast enough for our purpose
-            this.userList.push(user);
-            return 1;
-        },
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
 
-        find: function (id) {
-            if (id) {
-                return this.userList.find(function (element) {
-                    return element.id === id;
-                });
-            }
-            else {
-                return this.userList;
-            }
-        },
-        findOne: function (name) {
-            if (name) {
-                return this.userList.find(function (element) {
-                    return element.username === name;
-                });
-            }
-            else {
-                return this.userList;
-            }
-        },
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try
+{
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error)
+{
+    console.log("could not connect");
+}
+//mongoose.set('useCreateIndex', true);
 
-        remove: function (id) {
-            var found = 0;
-            this.userList = this.userList.filter(function (element) {
-                if (element.id === id) {
-                    found = 1;
-                }
-                else {
-                    return element.id !== id;
-                }
-            });
-            return found;
-        },
+//movies schema
+let MovieSchema = new Schema({
+    title: {type: String, required: true, index: { unique: true}},
+    yearReleased: {type: String, required: true},
+    genre: {type: String, required: true},
+    actors: [{actorName: String, characterName: String}]
+});
 
-        update: function (id, user) {
-            var userIndex = this.userList.findIndex(function (element) {
-                return element.id === id;
-            });
-            if (userIndex !== -1) {
-                this.userList[userIndex].username = user.username;
-                this.userList[userIndex].password = user.password;
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-    };
-};
+
+//return the model to server
+module.exports = mongoose.model('Movie', MovieSchema);
